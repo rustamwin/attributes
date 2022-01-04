a739c39f57b6133d57d2043de67abdd893e7a571
Rustam <rmamdaminov@gmail.com>
2022-01-04 13:46:58 +0500

    Minor

--- tests/Support/MockAttribute.php
+++ tests/Support/MockAttribute.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace RustamWin\Attributes\Tests\Support;
 
 use Attribute;
-use JetBrains\PhpStorm\Immutable;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 final class MockAttribute
