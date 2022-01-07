8e2afc0e14fdcd01e44284489ddf53c7fe310279
Rustam Mamadaminov <rmamdaminov@gmail.com>
2022-01-07 16:57:58 +0500

    Rename Presenter to Handler (#2)

--- tests/AttributesTest.php
+++ tests/AttributesTest.php
@@ -6,24 +6,24 @@ namespace RustamWin\Attributes\Tests;
 
 use PHPUnit\Framework\TestCase;
 use RustamWin\Attributes\Attributes;
-use RustamWin\Attributes\Presenter\CompositePresenter;
+use RustamWin\Attributes\Handler\CompositeHandler;
 use RustamWin\Attributes\Tests\Support\MockEntity;
-use RustamWin\Attributes\Tests\Support\SimplePresenter;
+use RustamWin\Attributes\Tests\Support\SimpleHandler;
 
 final class AttributesTest extends TestCase
 {
     public function testHandle(): void
     {
-        $this->assertContains('Hello, World!', SimplePresenter::getValues());
+        $this->assertContains('Hello, World!', SimpleHandler::getValues());
     }
 
     public function testWithCompositePresenter(): void
     {
-        $attributes = new Attributes(attributePresenter: new CompositePresenter([new SimplePresenter()]));
+        $attributes = new Attributes(attributeHandler: new CompositeHandler([new SimpleHandler()]));
 
         $attributes->setClasses([MockEntity::class])->setDirectories([__DIR__ . '/Support'])->handle();
 
-        $this->assertContains('Hello, World!', SimplePresenter::getValues());
+        $this->assertContains('Hello, World!', SimpleHandler::getValues());
 
     }
 }
